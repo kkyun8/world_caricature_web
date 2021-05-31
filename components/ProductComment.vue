7249ec721fda89bde414251e663464f0891bba9f
kkyun8 <kim23302@icloud.com>
2021-05-31 23:29:07 +0900

    product comment box作成

--- components/ProductComment.vue
+++ components/ProductComment.vue
@@ -1,9 +1,39 @@
 <template>
-  <div>comment</div>
+  <div class="py-6">
+    <div v-for="(c, ipc) in productComments" :key="`pc-${ipc}`" class="columns">
+      <div class="column mb-4">
+        <div class="box">
+          <article class="media">
+            <div class="media-left">
+              <!-- <img TODO: order img /> -->
+            </div>
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>{{ c.name.S }}</strong
+                  >様
+                  <br />
+                  {{ c.comment.S }}
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+export default {
+  name: 'ProductComment',
+  computed: {
+    ...mapState({
+      productComments: (state) => state.comment.productComments,
+    }),
+  },
+}
 </script>
 
 <style></style>
