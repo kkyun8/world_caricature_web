6f07e409bab847bb4028d03fbe3852907d2cba20
kkyun8 <kim23302@icloud.com>
2021-06-04 00:01:31 +0900

    artist login基本コンポーネント追加

---
+++ pages/platform/login/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container is-max-desktop my-6 p-3" style="height: 400px">
+    <section>
+      <div class="columns">
+        <div class="column is-half is-offset-one-quarter">
+          <div class="box has-text-centered mt-6">
+            <!-- TODO: icon or img
+                    -->
+
+            <b-button type="is-link is-light">ソーシャルログイン</b-button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlatformLogin',
+  layout: 'only_footer',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style></style>
